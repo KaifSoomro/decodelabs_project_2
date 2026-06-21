de047c6bff66960cd41c1a8c651246381d758c2d
KaifSoomro <kaifsoomro82@gmail.com>
2026-06-21 23:19:51 +0500

    Some Security and Routes

--- backend/routes/quote.route.js
+++ backend/routes/quote.route.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { addQuote, getAllQuotes } from "../controllers/quoteController.js";
 
 const router = express.Router();
 
 router.post("/post", addQuote);
-// router.get("/quotes", getAllQuotes);
+router.get("/get-all-quotes", getAllQuotes);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 import "dotenv/config";
 import express from "express";
 import connectDB from "./config/db.js";
+import quoteRoute from "./routes/quote.route.js";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 connectDB();
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+app.use(helmet());
 app.use(express.json());
+
+app.use("/api/v1/quotes", quoteRoute);
 
 app.get("/", (req, res) => {
   res.send("Quote API is running...");
